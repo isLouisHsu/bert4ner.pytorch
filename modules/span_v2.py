762ad0fda0580214f6bffa0a411f2f90cc5ded0e
louishsu <is.louishsu@foxmail.com>
2021-08-20 22:23:15 +0800

    修复不能复现问题

--- modules/span_v2.py
+++ modules/span_v2.py
@@ -6,20 +6,29 @@ import torch.nn.functional as F
 from torchtyping import TensorType
 
 # from allennlp.nn.util import batched_index_select
-def batched_index_select(target: torch.Tensor, indices: torch.LongTensor) -> torch.Tensor:
-    batch_size, sequence_length, hidden_size = target.size()
-    _, num_spans = indices.size()
-    indexed = []
-    for b in range(batch_size):
-        target_b = target[b]
-        indices_b = indices[b]
-        indexed_b = []
-        for i in range(num_spans):
-            indexed_b.append(target_b[indices_b[i]])
-        indexed_b = torch.stack(indexed_b, dim=0)
-        indexed.append(indexed_b)
-    indexed = torch.stack(indexed, dim=0)
-    return indexed
+# def batched_index_select(target: torch.Tensor, indices: torch.LongTensor) -> torch.Tensor:
+#     batch_size, sequence_length, hidden_size = target.size()
+#     _, num_spans = indices.size()
+#     indexed = []
+#     for b in range(batch_size):
+#         target_b = target[b]
+#         indices_b = indices[b]
+#         indexed_b = []
+#         for i in range(num_spans):
+#             indexed_b.append(target_b[indices_b[i]])
+#         indexed_b = torch.stack(indexed_b, dim=0)
+#         indexed.append(indexed_b)
+#     indexed = torch.stack(indexed, dim=0)
+#     return indexed
+def batched_index_select(input, index, dim=1):
+    for i in range(1, len(input.shape)):
+        if i != dim:
+            index = index.unsqueeze(i)
+    expanse = list(input.shape)
+    expanse[0] = -1
+    expanse[dim] = -1
+    index = index.expand(expanse)
+    return torch.gather(input, dim, index)
 
 class SpanV2(nn.Module):
     
